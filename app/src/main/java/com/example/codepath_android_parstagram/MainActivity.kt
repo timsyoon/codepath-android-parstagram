43e494a543f8011369c013d83df0c92da3da17ad
Timothy Yoon <timsyoon@gmail.com>
2022-03-23 21:45:51 -0700

    Install Parse (connection not working)

--- app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
+++ app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.codepath_android_parstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.ParseObject
 
+/**
+ * Let user create a post by taking a photo with their camera
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            }else{
+                Log.d("MainActivity","Object saved.")
+            }
+        }
     }
 }
