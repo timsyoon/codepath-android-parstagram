0c43753ff46a09c367744826e26a5d509c227002
Timothy Yoon <timsyoon@gmail.com>
2022-03-24 08:11:13 -0700

    Implement login feature

--- app/src/main/java/com/example/codepath_android_parstagram/LoginActivity.kt
+++ app/src/main/java/com/example/codepath_android_parstagram/LoginActivity.kt
@@ -1,14 +1,24 @@
 package com.example.codepath_android_parstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseObject
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Check if a user is logged in. If they are, take them to MainActivity
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
 
         findViewById<Button>(R.id.login_button).setOnClickListener {
             val username = findViewById<EditText>(R.id.et_username).text.toString()
@@ -18,6 +28,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
 
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val TAG = "LoginActivity"
     }
 }
--- app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
+++ app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
@@ -12,14 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val firstObject = ParseObject("FirstClass")
-        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-        firstObject.saveInBackground {
-            if (it != null){
-                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-            }else{
-                Log.d("MainActivity","Object saved.")
-            }
-        }
     }
 }
