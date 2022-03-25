8907f670828411e87d33807b1240315b6820a61c
Timothy Yoon <timsyoon@gmail.com>
2022-03-25 12:25:23 -0700

    Implement log out feature

--- app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
+++ app/src/main/java/com/example/codepath_android_parstagram/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.codepath_android_parstagram
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.parse.*
 import java.io.File
+
 
 /**
  * Let user create a post by taking a photo with their camera
@@ -53,6 +56,40 @@ class MainActivity : AppCompatActivity() {
         }
 
         // queryPosts()
+    }
+
+    // Source: https://guides.codepath.com/android/Defining-The-ActionBar#adding-action-items
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    // Source: https://guides.codepath.com/android/Defining-The-ActionBar#handling-actionbar-clicks
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+        if (item.getItemId() == R.id.miSignout) {
+            ParseUser.logOutInBackground { e ->
+                if (e == null) {
+                    goToLoginActivity()
+                    Log.i(TAG, "Sign out successful")
+                    Toast.makeText(this, "Sign out successful", Toast.LENGTH_SHORT)
+                } else {
+                    Log.e(TAG, "Sign out failed")
+                    Toast.makeText(this, "Sign out failed", Toast.LENGTH_SHORT)
+                }
+            }
+            val currentUser = ParseUser.getCurrentUser() // this will now be null
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     fun submitPost(description: String, user: ParseUser, file: File) {
