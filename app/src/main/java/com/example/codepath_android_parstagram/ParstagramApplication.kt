9425ba2d507cf62d3f91e25a10fd3f0db37688fc
Timothy Yoon <timsyoon@gmail.com>
2022-03-24 19:47:28 -0700

    Set up Parse data object and perform basic query for posts

--- app/src/main/java/com/example/codepath_android_parstagram/ParstagramApplication.kt
+++ app/src/main/java/com/example/codepath_android_parstagram/ParstagramApplication.kt
@@ -2,10 +2,12 @@ package com.example.codepath_android_parstagram
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParstagramApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        ParseObject.registerSubclass(Post::class.java)
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
